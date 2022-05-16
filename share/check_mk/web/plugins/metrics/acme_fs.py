b10cd73dc27a8edf6d34d0d19979ae9d18b92fc5
agabellini <andrea.gabellini@telecomitalia.sm>
2022-05-16 11:34:15 +0200

    Delete share/check_mk/web/plugins/metrics directory

--- share/check_mk/web/plugins/metrics/acme_fs.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-#
-# License: GNU General Public License v2
-
-from cmk.gui.plugins.metrics import check_metrics
-from cmk.gui.plugins.metrics.translation import df_translation
-
-check_metrics["check_mk-acme_fs"] = df_translation
